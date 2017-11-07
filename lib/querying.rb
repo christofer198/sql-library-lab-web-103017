2db8fcd4b9299eac000187346983115256957708
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 14:36:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "select species, sum(species) from characters order by asc sum(species)"
+  "select species, sum(species) from characters order by desc species"
 end
 
 def select_name_and_series_subgenres_of_authors
