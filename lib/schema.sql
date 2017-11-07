5e65861dc3913e9a6d6dced2c9f259c5b98a18ef
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 13:33:44 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,36 @@
+create table characters(
+  id integer primary key,
+  name text,
+  motto text, 
+  species text
+);
+
+create table series(
+  id integer primary key,
+  title text,
+  author text,
+  genre text  
+);
+
+create table Sub-Genres(
+  id integer primary key,
+  name text
+);
+
+create table authors(
+  id integer primary key,
+  name text
+);
+
+create table books(
+  id integer primary key,
+  title text,
+  year date,
+  series text
+);
+
+create table character_books(
+  id integer primary key,
+  book_id integer,
+  character_id integer
+)
